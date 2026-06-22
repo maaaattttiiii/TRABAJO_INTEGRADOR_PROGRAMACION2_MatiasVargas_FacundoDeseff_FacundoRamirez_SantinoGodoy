e83f1b67eeebbeb021ba99c112dacce22a7baf20
Santino Godoy <santinogodoygaldeano@gmail.com>
2026-06-21 21:28:44 -0300

    Agrego 2 posibles excepciones.

--- src/integrador/prog2/exception/EntidadNoEncontradaException.java
+++ src/integrador/prog2/exception/EntidadNoEncontradaException.java
@@ -1,5 +1,9 @@
 package integrador.prog2.exception;
 
+/**
+ * Se lanza cuando se intenta buscar una entidad
+ * que no existe en el sistema.
+ */
 public class EntidadNoEncontradaException extends Exception {
     public EntidadNoEncontradaException(String mensaje) {
         super(mensaje);
--- src/integrador/prog2/exception/PersistenciaException.java
+++ src/integrador/prog2/exception/PersistenciaException.java
@@ -1,5 +1,9 @@
 package integrador.prog2.exception;
 
+/**
+ * Se lanza cuando ocurre un error al acceder
+ * o guardar información en la base de datos.
+ */
 public class PersistenciaException extends Exception {
 
     public PersistenciaException(String mensaje) {
